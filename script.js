f11fa17798b7b18332402d1bb52e738ee1a1ed4a
Mohammed Rafeeq <124717010+Mohammed-Rafeeq13@users.noreply.github.com>
2024-06-07 22:52:10 +0530

    script.js

--- script.js
+++ script.js
@@ -14,3 +14,66 @@ let mainImg = document.getElementById("mainImg");
         smalImg[3].onclick = function(){
             mainImg.src =smalImg[3].src;
         }
+
+document.getElementById('registrationForm').addEventListener('submit',function(event) {
+	event.preventDefault(); // Prevent the default behaviour or form submission
+
+	//clear previous errors
+	clearErrors();
+
+	//get the values from each input field or form
+	const name = document.getElementById('name').value;
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+
+	let isValid = true;
+
+	//validation for name(letters and spaces)
+	if(!/^[a-zA-Z\s]+$/.test(name.trim())){
+		setError('name','Name must contain only letters and spaces');
+		isValid = false;
+	}
+	//validation for email , valid email @
+	if (!validate_email(email)){
+		setError('email','Please enter a valid email');
+		isValid = false;
+
+	}
+	//validation for password there should be a minimum length of 8 
+	if (password.length < 6) {
+        setError('password', "Password must be at least 6 characters");
+        isValid = false;
+    }
+
+
+	if(isValid){
+		alert("Form Submitted Successfully");
+	}
+});
+
+
+
+function setError(id,message){
+	const element = document.getElementById(id);
+	const small = element.nextElementSibling;
+	element.classList.add('error');
+	small.innerText = message;
+	small.style.visibility = 'visible';
+}
+
+function clearErrors()
+{
+	const inputs = document.querySelectorAll('input');
+	inputs.forEach(input => {
+		input.classList.remove('error');
+		const small = input.nextElementSibling;
+		small.style.visibility = 'hidden';
+	});
+}
+
+function validate_email(email){
+	const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return re.test(String(email).toLowerCase());
+}
+
+
